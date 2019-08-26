31e2709a0c42ed6330eadd3c8b63333fad36a587
GRÉGOIRE BUSSIÈRE <38251729+gregoirebussiere@users.noreply.github.com>
2019-08-26 14:30:02 +0200

    Delete N200_m_cond.m

--- N200_m_cond.m
+++
@@ -1,5 +0,0 @@
-function [N200_m] = N200_m_cond(data,cond,ind,N200_window,N200_subset)
-    study = condition_selection(data,cond,ind); %selection of condition
-    N200_me = mean(study(N200_window,N200_subset,:),2); %mean across the electrodes
-    N200_m = mean(squeeze(N200_me),2); % mean across electrodes and trials
-end
